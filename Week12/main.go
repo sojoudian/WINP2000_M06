c7704c00007ea091c4fc21f62534f4f99d2771b6
maziar <sojodyan@gmail.com>
2024-11-21 10:19:50 -0500

    update main with removing the comments

--- Week12/main.go
+++ Week12/main.go
@@ -1,17 +1,10 @@
 package main
-
 import (
 	"log"
 	"net/http"
 )
-
 func main() {
-
-	// handle `/` route
 	fs := http.FileServer(http.Dir("./templates"))
 	http.Handle("/", fs)
-
-	// run server on port "80"
 	log.Fatal(http.ListenAndServe(":80", nil))
-
 }
