e63e9428c1281bfb7c598f0a0f821cb97e55ad87
maziar <sojodyan@gmail.com>
2024-11-21 11:19:17 -0500

    add all req

--- Week12/main.go
+++ Week12/main.go
@@ -12,8 +12,13 @@ import (
 
 var mongoClient *mongo.Client
 
+func enablCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
 func saveIPHandler(w http.ResponseWriter, r *http.Request){
-	// TODO fix FW issues
+	enablCors(&w)
 	if r.Method != "POST" {
 		http.Error(w,"Only POST method is allowed.", http.StatusMethodNotAllowed)
 		return
@@ -27,18 +32,22 @@ func saveIPHandler(w http.ResponseWriter, r *http.Request){
 	currentTime := time.Now().Format("2006-01-02 15:04:05")
 	data["time"] = currentTime
 	collection := mongoClient.Database("portfolio").Collection("ips")
-	_, err := collection.InsertOne(context.TODO(), data) 
+	_, err = collection.InsertOne(context.TODO(), data) 
+	if err != nil {
+		http.Error(w, "Error saving data to MongoDB", http.StatusInternalServerError)
+	}
+
 }
 func main() {
 	var err error
-	mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	mongoClient, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fs := http.FileServer(http.Dir("./templates"))
 	http.Handle("/", fs)
-	http.HandleFunc("/vistor")
-
+	http.HandleFunc("/api/vistors", saveIPHandler)
+	log.Println("Server started on port 80")
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
